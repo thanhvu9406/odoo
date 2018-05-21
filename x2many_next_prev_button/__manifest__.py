ab7130b95c89c73c6e0f72d4a360f7f0e822e9a1
Vu <vunt@trobz.com>
2018-05-21 17:06:12 +0700

    Refactor module

---
+++ x2many_next_prev_button/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "x2many_next_prev_button",
+
+    'summary': """
+        add next - previous button to popup x2many lines
+        """,
+    "description": """
+    Add next - previous button to form popup for one2many, many2many fields line.
+""",
+
+    'author': "Nguyễn Thành Vũ",
+
+    'category': 'Dependency',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['web'],
+
+    # always loaded
+    'data': [
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
